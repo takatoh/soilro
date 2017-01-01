560b6c90f3eed75d81cc858cd9e79ffc8da00b17
takatoh <takatoh.m@gmail.com>
2017-01-01 15:13:53 +0900

    v0.5.2

--- main.hs
+++ main.hs
@@ -10,7 +10,7 @@ import qualified InputDataParser as P
 --------------------------------------------------------------------------------
 
 progName = "soilro"
-version  = "v0.5.1"
+version  = "v0.5.2"
 
 
 main :: IO ()
