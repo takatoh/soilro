15600c162eda1c5b57e1ad97fd69504626ed1ad5
takatoh <takatoh.m@gmail.com>
2017-03-24 20:43:56 +0900

    main.hs: Transform formula in modified R-O model.

--- main.hs
+++ main.hs
@@ -169,7 +169,7 @@ modifiedRoModel input gamma = (gamma, gRatio, h)
 calcGRatioModified :: D.Gamma -> D.Gamma -> Double -> Double
 calcGRatioModified g gh b = bisectionMethod f 0.0 1.0
   where
-    f x = x - 1.0 / (1.0 + 2.0 ** b * (x * g / gh) ** b)
+    f x = x * (1.0 + (2.0 * x * (g / gh)) ** b) - 1.0
 
 --------------------------------------------------------------------------------
 
