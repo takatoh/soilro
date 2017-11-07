87ea1e83b1dd821743735eccef17c878500f0c04
takatoh <takatoh.m@gmail.com>
2017-11-07 21:23:48 +0900

    Revert "main.hs: Error handling for parsing input data."
    This reverts commit 2dd67876c0e8f357022a76d04a0a1db77c649efb.

--- main.hs
+++ main.hs
@@ -10,7 +10,7 @@ import qualified InputDataParser as P
 --------------------------------------------------------------------------------
 
 progName = "soilro"
-version  = "v0.7.0"
+version  = "v0.7.1"
 
 
 main :: IO ()
@@ -22,12 +22,9 @@ main = do argv <- getArgs
             putStrLn $ usageInfo header options
           else do cs <- readFile (head n)
                   let input = P.parseInputData cs
-                  case input of
-                    D.ParseErr e      -> print e
-                    D.InputData g h p ->
-                      let model = genModel (optModelType o) input in
-                      let format = genFormatter (optOutputFormat o) in
-                      putStr $ format $ map model $ D.iPlotG input
+                  let model = genModel (optModelType o) input
+                  let format = genFormatter (optOutputFormat o)
+                  putStr $ format $ map model $ D.iPlotG input
 
 
 --------------------------------------------------------------------------------
@@ -143,7 +140,7 @@ roModel input gamma = (gamma, gRatio, h)
 calcGRatio :: D.Gamma -> D.Gamma -> Double -> Double
 calcGRatio g gh b = bisectionMethod f 0.0 1.0
   where
-    f x = x * (1 + (2 * x * (g / gh)) ** (b - 1)) - 1
+    f x = x * (1.0 + (2.0 * x * (g / gh)) ** (b - 1.0)) - 1.0
 
 
 -- H-D model
@@ -172,7 +169,7 @@ modifiedRoModel input gamma = (gamma, gRatio, h)
 calcGRatioModified :: D.Gamma -> D.Gamma -> Double -> Double
 calcGRatioModified g gh b = bisectionMethod f 0.0 1.0
   where
-    f x = x - 1.0 / (1.0 + 2.0 ** b * (x * g / gh) ** b)
+    f x = x * (1.0 + (2.0 * x * (g / gh)) ** b) - 1.0
 
 --------------------------------------------------------------------------------
 
