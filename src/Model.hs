a8c34dbbc234b7b817c924520882176867f58d2e
takatoh <takatoh.m@gmail.com>
2026-03-14 06:44:39 +0900

    src/Model.hs: Use function 'bisectionMethod' from src/Util.hs.

--- src/Model.hs
+++ src/Model.hs
@@ -1,6 +1,7 @@
 module Model where
 
 import qualified DataDef as D
+import Util (bisectionMethod)
 
 --------------------------------------------------------------------------------
 
@@ -38,22 +39,5 @@ hdModel input gamma = (gamma, gRatio, h)
     hmax   = D.iHMax input
     gRatio = 1.0 / (1.0 + gamma / gammaH)
     h      = hmax * (1 - gRatio)
-
---------------------------------------------------------------------------------
-
--- Bisection Method
--- ATTENTION:  f x1 < 0 < f x2
-
-bisectionMethod :: (Double -> Double) -> Double -> Double -> Double
-bisectionMethod f x1 x2 =
-  let xm = (x1 + x2) / 2
-      y = f xm
-  in
-  if abs y < 1.0e-12 then
-    xm
-  else if y < 0 then
-    bisectionMethod f xm x2
-  else
-    bisectionMethod f x1 xm
 
 --------------------------------------------------------------------------------
